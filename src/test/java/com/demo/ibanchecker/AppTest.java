97b1bcd52e6d3e3dec5b85364f14f56be9114a31
esure-zjankunas <zilvinas.jankunas@esure.com>
2019-06-25 21:18:05 +0100

    Test

--- src/test/java/com/demo/ibanchecker/AppTest.java
+++ src/test/java/com/demo/ibanchecker/AppTest.java
@@ -1,5 +1,11 @@
 package com.demo.ibanchecker;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,17 +13,17 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest extends TestCase
 {
     /**
      * Create the test case
      *
-     * @param testName name of the test case
+     * @param testName
+     *            name of the test case
      */
-    public AppTest( String testName )
+    public AppTest(String testName)
     {
-        super( testName );
+        super(testName);
     }
 
     /**
@@ -25,14 +31,25 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
     /**
-     * Rigourous Test :-)
+     * Test a few IBANS
+     * 
+     * @throws IOException
      */
-    public void testApp()
+    public void testApp() throws IOException
     {
-        assertTrue( true );
+
+        List<String> lines = Files.lines(Paths.get("src/main/resources/ibans.txt")).collect(Collectors.toList());
+        assert (!App.validateIBAN(lines.get(0)));
+        assert (App.validateIBAN(lines.get(1)));
+        assert (App.validateIBAN(lines.get(2)));
+        assert (!App.validateIBAN(lines.get(3)));
+        assert (!App.validateIBAN(lines.get(4)));
+        assert (!App.validateIBAN("zzz"));
+        assert (!App.validateIBAN("ZA123567890-"));
+
     }
 }
